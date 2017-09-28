d7d3d2884907cadb1ec170a5f5830aa960e54286
Pauline <bootaxxxx@gmail.com>
2017-09-28 13:23:48 +0800

    Waiting Time, Average Computing Time : FCFS,SJF,Priority

--- lab2.py
+++ lab2.py
@@ -9,9 +9,12 @@ class Process(object):
         self.arrival = arrival
         self.burst = burst
         self.priority = priority
+        self.waiting = 0
 
     def __str__(self):
-        return ','.join([self.name, self.arrival, self.burst, self.priority])
+        str1 = str(self.name) + "," + str(self.arrival)
+        str2 = str(self.burst) + "," + str(self.priority)
+        return ','.join([str1, str2, str(self.waiting)])
 
 
 class Schedule(object):
@@ -19,6 +22,25 @@ class Schedule(object):
     def __init__(self, processes):
         super(Schedule, self).__init__()
         self.processes = processes
+        self.aveCT = 0
+
+    def averageCT(self):
+        sum = 0
+        for process in self.processes:
+            sum += process.waiting
+        self.aveCT = sum / len(self.processes)
+
+    def genWaiting(self):
+        prev = self.processes[0]
+        for x in range(1, len(self.processes)):
+            self.processes[x].waiting = prev.waiting + prev.burst
+            prev = self.processes[x]
+
+    def resetSched(self):
+        reset = sorted(self.processes, key=lambda process: process.name)
+        for process in reset:
+            process.waiting = 0
+        self.processes = reset
 
     def __str__(self):
         return self.processes
@@ -35,6 +57,10 @@ class SJF(Schedule):
     def __init__(self, processes):
         Schedule.__init__(self, processes)
 
+    def sortSched(self):
+        sortedsched = sorted(self.processes, key=lambda process: process.burst)
+        self.processes = sortedsched
+
 
 class SRPT(Schedule):
     """docstring for FCFS"""
@@ -46,6 +72,10 @@ class Priority(Schedule):
     """docstring for FCFS"""
     def __init__(self, processes):
         Schedule.__init__(self, processes)
+
+    def sortSched(self):
+        ssched = sorted(self.processes, key=lambda process: process.priority)
+        self.processes = ssched
 
 
 class RoundRobin(Schedule):
@@ -62,6 +92,9 @@ class FileRead(object):
         self.lines = []
         self.processes = []
 
+    def getProcesses(self):
+        return self.processes
+
     def extractData(self):
         string = open("processes/process1.txt", "r")
         for line in string:
@@ -74,28 +107,54 @@ class FileRead(object):
                 flag = False
             else:
                 split = line.split()
-                proc = Process(split[0], split[1], split[2], split[3])
+                name = int(split[0])
+                arrival = int(split[1])
+                burst = int(split[2])
+                priority = int(split[3])
+                proc = Process(name, arrival, burst, priority)
                 self.processes.append(proc)
 
 
 def main():
-    print("Greed")
-    schedule = Schedule("schedule")
-    fcfs = FCFS("fcfs")
-    sjf = SJF("sjf")
-    srpt = SRPT("srpt")
-    priority = Priority("priority")
-    roundrobin = RoundRobin("roundrobin")
     file = FileRead("/process1.txt")
     file.extractData()
     file.datatoProcess()
+    processes = file.getProcesses()
 
-    print(schedule)
-    print(fcfs)
-    print(sjf)
-    print(priority)
-    print(roundrobin)
-    print(file.processes[1])
+    # FCFS------------------------------------------------------------------------------------------
+    fcfs = FCFS(processes)
+    fcfs.genWaiting()
+    for process in fcfs.processes:
+        print(process)
+    fcfs.averageCT()
+    print("Average Waiting Time: " + str(fcfs.aveCT))
+
+    # SJF-------------------------------------------------------------------------------------------
+    sjf = SJF(processes)
+    sjf.resetSched()
+    sjf.sortSched()
+    sjf.genWaiting()
+    for process in sjf.processes:
+        print(process)
+    sjf.averageCT()
+    print("Average Waiting Time: " + str(sjf.aveCT))
+
+    # SRPT-------------------------------------------------------------------------------------------
+    srpt = SRPT(processes)
+
+    # PRIORITY---------------------------------------------------------------------------------------
+    priority = Priority(processes)
+    priority.resetSched()
+    priority.sortSched()
+    priority.genWaiting()
+    for process in priority.processes:
+        print(process)
+    priority.averageCT()
+    print("Average Waiting Time: " + str(priority.aveCT))
+
+    # ROUNDROBIN-------------------------------------------------------------------------------------
+    roundrobin = RoundRobin(processes)
+
 
 if __name__ == '__main__':
     main()
